40418a1bd31eac64bbc16ff46aee359a08115b37
Max Parastiuk <parastukm@gmail.com>
2021-04-04 14:29:39 +0300

    MASTER-1: template third

---
+++ src/homework5/task1/Zoo.java
@@ -0,0 +1,21 @@
+package homework5.task1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Zoo {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add(0, "dog");
+        list.add(1, "cat");
+        list.add(2, "parrot");
+        list.add(3, "hamster");
+        list.add(4, "mouse");
+        list.add(5, "cow");
+        list.add(6, "bull");
+        list.add(7, "pig");
+
+        System.out.println(list);
+    }
+}
