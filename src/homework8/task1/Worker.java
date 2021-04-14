9096a707fa0b627fb802df93e9d6bf17a717c709
Max Parastiuk <parastukm@gmail.com>
2021-04-14 14:55:04 +0300

    MASTER-1 : template seventh

--- src/homework8/task1/Worker.java
+++ src/homework8/task1/Worker.java
@@ -1,8 +1,6 @@
 package homework8.task1;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
--- src/homework8/task2/Price.java
+++ src/homework8/task2/Price.java
@@ -1,7 +1,5 @@
 package homework8.task2;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 class Main {
