bac18386f56df3bfa855d087242e773b3b5727d2
Max Parastiuk <parastukm@gmail.com>
2021-04-18 17:10:11 +0300

    MASTER-1 : template ninth

---
+++ src/homework10/task4/ArrayIterator.java
@@ -0,0 +1,20 @@
+package homework10.task4;
+
+import java.util.Iterator;
+
+public class ArrayIterator<E> implements Iterator<E> {
+    private int index;
+    private E[] values;
+    public ArrayIterator(E[] values) {
+        this.values = values;
+    }
+
+    @Override
+    public boolean hasNext(){
+        return index < values.length;
+    }
+    @Override
+    public E next(){
+        return values[index++];
+    }
+}
--- src/homework10/task4/List.java
+++ src/homework10/task4/List.java
@@ -1,35 +1,10 @@
 package homework10.task4;
 
-import java.lang.reflect.Type;
-import java.util.ArrayList;
+public interface List<E> extends Iterable<E>  {
+    boolean add(E e);
+    void remove(int index);
+    E get(int index);
+    int size();
+    void set(int index, E e);
 
-public class List {
-    public static void main(String[] args) {
-        
-        ArrayList<String> list = new ArrayList<>();
-        list.add("Pig");
-        list.add("Hamster");
-        list.add("Wolf");
-
-        System.out.println(list);
-
-        list.set(1, "Dog");
-        System.out.println(list);
-
-        System.out.println(list.get(1));
-
-        list.remove(1);
-        System.out.println(list);
-
-        System.out.println(list.indexOf("Wolf"));
-
-        System.out.println(list.size());
-
-        System.out.println(list.toString());
-
-        list.clear();
-        System.out.println(list.size());
-
-        System.out.println(list.isEmpty());
-    }
 }
